f822e37156ad103d156d61c8ce89951d33deb05c
rvillabonas <rvillabonas@717f917d-fdd7-54f2-cfaa-ef75c8cdf1c5>
2011-05-16 03:00:03 +0000

    Data Access Object  (All views and All tables)

---
+++ ParqueaderoCarRun/src/java/DAO/Contrato.java
@@ -0,0 +1,31 @@
+package DAO;
+
+import java.util.List;
+
+/**
+ *
+ * @author eagle
+ */
+public class Contrato  extends GeneralDAO<Contrato, String> {
+
+    @Override
+    public String getReadQuery() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getReadByNameQuery() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String[] makeArray(List lis, int tam) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Class getEntityClass() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
