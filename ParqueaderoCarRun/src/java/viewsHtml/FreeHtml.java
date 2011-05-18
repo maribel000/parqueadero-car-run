c2f8e5caa12fe5cab26ccbaa9a4fc588ad44e233
rvillabonas <rvillabonas@717f917d-fdd7-54f2-cfaa-ef75c8cdf1c5>
2011-05-18 12:03:28 +0000

    metodos par el diseño de las vistas

---
+++ ParqueaderoCarRun/src/java/viewsHtml/FreeHtml.java
@@ -0,0 +1,42 @@
+
+package viewsHtml;
+
+/**
+ *
+ * @author eagle
+ */
+public class FreeHtml {
+    
+   private String htmltext;
+
+    /**
+     * @return the htmltext
+     */
+    public String getHtmltext() {
+        return htmltext;
+    }
+
+    /**
+     * @param htmltext the htmltext to set
+     */
+    public void setHtmltext(String htmltext) {
+        this.htmltext = htmltext;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
